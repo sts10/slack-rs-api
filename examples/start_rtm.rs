ae0c40c7cf79d3b05d23218853c09ee1f8cbc3d2
Ben Kimock <kimockb@gmail.com>
2018-06-16 00:29:20 -0400

    polish up start_rtm for debugging

--- examples/start_rtm.rs
+++ examples/start_rtm.rs
@@ -1,10 +1,8 @@
-extern crate futures;
 extern crate slack_api as slack;
-use futures::Future;
 
 use std::default::Default;
 use std::env;
-
+use std::error::Error;
 fn main() {
     let token = env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN not set.");
     let client = slack::default_client();
@@ -13,20 +11,24 @@ fn main() {
         let request = slack::rtm::StartRequest::default();
         let response = slack::rtm::start(&client, &token, &request);
 
-        if let Ok(response) = response {
-            if let Some(channels) = response.channels {
-                let channels = channels.into_iter().map(|c| c.name).collect::<Vec<_>>();
+        match response {
+            Ok(response) => {
+                //println!("{:#?}", response.others);
+                if let Some(channels) = response.channels {
+                    let channels = channels.into_iter().map(|c| c.name).collect::<Vec<_>>();
 
-                println!("Got channels: {}", channels.join(", "));
+                    println!("Got channels: {}", channels.join(", "));
+                }
+
+                if let Some(users) = response.users {
+                    let users = users.into_iter().map(|u| u.name).collect::<Vec<_>>();
+
+                    println!("Got users: {}", users.join(", "));
+                }
             }
-
-            if let Some(users) = response.users {
-                let users = users.into_iter().map(|u| u.name).collect::<Vec<_>>();
-
-                println!("Got users: {}", users.join(", "));
+            Err(e) => {
+                println!("{}\n{:?}", e.description(), e.cause());
             }
-        } else {
-            println!("{:?}", response);
         }
     }
 }
